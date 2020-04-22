8182917262c7fac2fcae5b98c3d7c7ed38fa61ad
EDUARDO LOMBARDI <eduardolomb@gmail.com>
2020-04-22 00:17:55 -0300

    Adição do layout

--- MarvelApp/MarvelApp/Controllers/CollectionViews/MainCollectionViewController.swift
+++ MarvelApp/MarvelApp/Controllers/CollectionViews/MainCollectionViewController.swift
@@ -12,78 +12,97 @@ private let reuseIdentifier = "Cell"
 
 class MainCollectionViewController: UICollectionViewController {
 
+    let searchController = UISearchController(searchResultsController: nil)
+      
+      var filteredHeroes: [String] = []
+      var heroes: [String] = []
+      var isSearchBarEmpty: Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+      }
+      
+      var isFiltering: Bool {
+        return searchController.isActive && !isSearchBarEmpty
+      }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
+        configureSearchBar()
+        configureNavigationBar()
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func configureSearchBar() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Buscar Herois"
+
+        navigationItem.searchController = searchController
+        navigationItem.titleView?.backgroundColor = UIColor.white
+        navigationItem.titleView?.tintColor = UIColor.white
+        definesPresentationContext = true
+    }
+    
+    func configureNavigationBar() {
+        let app = UINavigationBarAppearance()
+        app.backgroundColor = UIColor.systemRed.withAlphaComponent(0.45)
+        self.navigationController?.navigationBar.scrollEdgeAppearance = app
+        navigationController?.navigationBar.backgroundColor = UIColor.systemRed
+
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 0
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        if isFiltering {
+                return filteredHeroes.count
+              }
+              return heroes.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+    //        let candy: Candy
+    //        if isFiltering {
+    //          candy = filteredHeroes[indexPath.row]
+    //        } else {
+    //          candy = heroes[indexPath.row]
+    //        }
+
     
         return cell
     }
 
     // MARK: UICollectionViewDelegate
 
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
         return true
     }
-    */
+    
 
     /*
-    // Uncomment this method to specify if the specified item should be selected
+     Uncomment this method to specify if the specified item should be selected
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         return true
     }
     */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    func filterContentForSearchText(_ searchText: String) {
     }
-    */
 
+}
+
+extension MainCollectionViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    let searchBar = searchController.searchBar
+    guard let searchText = searchBar.text else { return }
+    filterContentForSearchText(searchText)
+  }
 }
